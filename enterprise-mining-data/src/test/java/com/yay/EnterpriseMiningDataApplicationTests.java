ca8f1c31c57f12d2ebf18a2cb054e8fc140fd722
yuananyun <598775508@qq.com>
2017-07-29 21:05:58 +0800

    新建代码工程

---
+++ enterprise-mining-data/src/test/java/com/yay/EnterpriseMiningDataApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yay;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EnterpriseMiningDataApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
